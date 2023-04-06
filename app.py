1ef616afde58d84c3b1a5494bdca978161a18f38
“Shravani-Jeurkar” <shravanimjeurkar@gmail.com>
2023-04-06 15:21:13 +0530

    Updated files, changed ML alogorithm to Random Forest Regression

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pickle
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
-# print(model)
+model = pickle.load(open('place_predict.pkl', 'rb'))
+print(model)
 
 @app.route('/')
 def home():
@@ -16,12 +16,16 @@ def predict():
     data = [int(x) for x in request.form.values()]
     # print(list(request.form.values()))
     val = model.predict([data])
+    res = val[0]*100
+    print(res)
     val_str = ''
-    if val == 0:
+    if res < 50:
+        res = str(res)
         val_str = "You need to prepare well!!"
     
-    elif val == 1:
-        val_str = "All the very best, you are doing well!!"
+    elif res >49:
+        res = str(res)
+        val_str = "All the very best, you are doing well!! Your placement chances are" + res + "%"
     
     else:
         val_str = "Something went wrong! Try again later."
